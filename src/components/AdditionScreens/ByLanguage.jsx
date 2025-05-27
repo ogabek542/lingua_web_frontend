ee7e2d0cec6da932547ab35faa08cad3d667ff76
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2025-05-27 11:02:34 +0500

    added language section

--- src/components/AdditionScreens/ByLanguage.jsx
+++ src/components/AdditionScreens/ByLanguage.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import AzeriLanguage from "../../assets/svg/azeri.svg";
+import KazakLanguage from "../../assets/svg/kazak.svg";
+import KyrgizLanguage from "../../assets/svg/kygiz.svg";
+import TajikLanguage from "../../assets/svg/tajik.svg";
 
- const ByLanguage = () => {
+const ByLanguage = () => {
   return (
-    <div>
-      ByLanguage not
-    </div>
-  )
-}
+    <div className="flex flex-col  px-3 h-full  w-full">
+      {/* top contetn */}
+      <span className="text-md text-gray-500 my-2">Language</span>
+      {/* main content */}
+      <div className="w-full grid grid-cols-1 lg:grid-cols-3 grid-rows-6 gap-1 h-auto">
+        {/* azeri */}
+        <a
+          href="#"
+          className="rounded-xl flex items-center gap-2 hover:bg-gray-100 p-2 transition-colors"
+        >
+          <div className="w-[30px] h-[30px] rounded-md flex items-center justify-center">
+            <img src={AzeriLanguage} alt="azeri_language" />
+          </div>
+          <p className="text-md font-[400] capitalize">Azeribaijan</p>
+        </a>
+        {/* kazak */}
+        <a
+          href="#"
+          className="rounded-xl flex items-center gap-2 hover:bg-gray-100 p-2 transition-colors"
+        >
+          <div className="w-[30px] h-[30px] rounded-md flex items-center justify-center">
+            <img src={KazakLanguage} alt="azeri_language" />
+          </div>
+          <p className="text-md font-[400] capitalize">Kazakistan</p>
+        </a>
+        {/* kyrgiz */}
+        <a
+          href="#"
+          className="rounded-xl flex items-center gap-2 hover:bg-gray-100 p-2 transition-colors"
+        >
+          <div className="w-[30px] h-[30px] rounded-md flex items-center justify-center">
+            <img src={KyrgizLanguage} alt="azeri_language" />
+          </div>
+          <p className="text-md font-[400] capitalize">Kyrgizistan</p>
+        </a>
+        {/* tajik */}
+        <a
+          href="#"
+          className="rounded-xl flex items-center gap-2 hover:bg-gray-100 p-2 transition-colors"
+        >
+          <div className="w-[30px] h-[30px] rounded-md flex items-center justify-center">
+            <img src={KyrgizLaTajikLanguagenguage} alt="azeri_language" />
+          </div>
+          <p className="text-md font-[400] capitalize">Tadjikistan</p>
+        </a>
 
-export default ByLanguage
+      </div>
+      {/* divider */}
+      <div className="w-full border-[1px] border-gray-200 my-2"></div>
+      {/* bottom content */}
+      <span className="text-md text-gray-500 my-2">See all language</span>
+    </div>
+  );
+};
+
+export default ByLanguage;
--- src/components/IndustryComponentData/AllDocumentsData.jsx
+++ src/components/IndustryComponentData/AllDocumentsData.jsx
@@ -1,150 +1,161 @@
 import { IoDocumentTextOutline } from "react-icons/io5";
-import { TbFileCertificate } from "react-icons/tb";
-import { BsHeartPulse } from "react-icons/bs";
-import { FiShield } from "react-icons/fi";
-import { TfiWrite } from "react-icons/tfi";
-import { BsHouseDoor } from "react-icons/bs";
-import { TbRubberStamp } from "react-icons/tb";
-import { RiGovernmentLine } from "react-icons/ri";
-import { TbReceiptTax } from "react-icons/tb";
-import { GoLaw } from "react-icons/go";
-import { BsCashCoin } from "react-icons/bs";
-import { LiaCertificateSolid } from "react-icons/lia";
-import { LuCar } from "react-icons/lu";
-import { GiInjustice } from "react-icons/gi";
 import { PiGraduationCapBold } from "react-icons/pi";
-import { LiaFileMedicalAltSolid } from "react-icons/lia";
+import { TbGrave2 } from "react-icons/tb";
 import { GiLovers } from "react-icons/gi";
-import { HiOutlineClipboardDocumentCheck } from "react-icons/hi2";
-
+import { LiaSignatureSolid } from "react-icons/lia";
+import { LiaBookMedicalSolid } from "react-icons/lia";
+import { BiCertification } from "react-icons/bi";
+import { BsHeartbreak } from "react-icons/bs";
+import { TbEPassport } from "react-icons/tb";
+import { BsBank2 } from "react-icons/bs";
+import { LuCar } from "react-icons/lu";
+import { HiOutlineDocumentText } from "react-icons/hi";
+import { LiaBabyCarriageSolid } from "react-icons/lia";
+import { RiMailSendLine } from "react-icons/ri";
+import { TbReceiptTax } from "react-icons/tb";
+import { IoMdBook } from "react-icons/io";
+import { GrContactInfo } from "react-icons/gr";
+import { MdOutlineVaccines } from "react-icons/md";
 
 
 
   const AllDocumentsScreensData = [
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: IoDocumentTextOutline,
-      contentText: "Academic Research",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: PiGraduationCapBold,
+      contentText: "Academic Transcripts",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: TbFileCertificate,
-      contentText: "Educational Credential Evaluation",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: TbGrave2,
+      contentText: "Death Certificate",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: BsHeartPulse,
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: GiLovers,
+      contentText: "Marriage Certificate",
+      linkAddress: "#",
+    },
+
+    {
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: LiaSignatureSolid,
+      contentText: "Affidavit",
+      linkAddress: "#",
+    },
+
+    {
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: PiGraduationCapBold,
+      contentText: "Diploma",
+      linkAddress: "#",
+    },
+
+    {
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: LiaBookMedicalSolid,
       contentText: "Medical Records",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: FiShield,
-      contentText: "Asylum Applications",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: BiCertification,
+      contentText: "Apostille",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: TfiWrite,
-      contentText: "Employment Abroad",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: BsHeartbreak,
+      contentText: "Divorce Documents",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: BsHouseDoor,
-      contentText: "Real Estate Transactions",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: TbEPassport,
+      contentText: "Passport",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: TbRubberStamp,
-      contentText: "Citizenship Applications",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: BsBank2,
+      contentText: "Bank Statement",
       linkAddress: "#",
     },
+
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: RiGovernmentLine,
-      contentText: "Government Filings",
-      linkAddress: "#",
-    },
-    {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: TbReceiptTax,
-      contentText: "Tax Documents",
-      linkAddress: "#",
-    },
-    {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: GoLaw,
-      contentText: "Deportation Defence",
-      linkAddress: "#",
-    },
-    {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: BsCashCoin,
-      contentText: "Inheritance Claims",
-      linkAddress: "#",
-    },
-    {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: LiaCertificateSolid,
-      contentText: "USCIS Translations",
-      linkAddress: "#",
-    },
-    {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
       IconName: LuCar,
-      contentText: "Driver's License Conversion",
+      contentText: "Driver's License",
+      linkAddress: "#",
+    },
+
+    {
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: HiOutlineDocumentText,
+      contentText: "Resume",
+      linkAddress: "#",
+    },
+
+    {
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: LiaBabyCarriageSolid,
+      contentText: "Birth Certificate",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: GiInjustice,
-      contentText: "Legal Proceedings",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: RiMailSendLine,
+      contentText: "Email",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: PiGraduationCapBold,
-      contentText: "University & College Admissions",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: TbReceiptTax,
+      contentText: "Tax Returns",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: LiaFileMedicalAltSolid,
-      contentText: "ECFMG Translations",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: IoMdBook,
+      contentText: "Brochure",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: GiLovers,
-      contentText: "Marriage Abroad",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: GrContactInfo,
+      contentText: "Legal Contact",
       linkAddress: "#",
     },
     {
-      iconbgColor: "rgba(229,233,224,1)",
-      iconColor: "rgba(147,158,130,1)",
-      IconName: HiOutlineClipboardDocumentCheck,
-      contentText: "Visa Applications",
+      iconbgColor: "rgba(230,238,245,1)",
+      iconColor: "rgba(53,116,184,1)",
+      IconName: MdOutlineVaccines,
+      contentText: "Vaccination Record",
       linkAddress: "#",
     },
 
