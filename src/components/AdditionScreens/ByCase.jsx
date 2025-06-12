f2c15102ad574eadc97be04eb8c4e42f810346f0
Og'abek542 <123234816+ogabek542@users.noreply.github.com>
2025-06-12 16:53:49 +0500

    add some cases

--- src/components/AdditionScreens/ByCase.jsx
+++ src/components/AdditionScreens/ByCase.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import IconContent from '../IconContent/IconContent';
-import CaseScreensData from '../IndustryComponentData/CaseIndustryData';
+import React from "react";
+import IconContent from "../IconContent/IconContent";
+import CaseScreensData from "../IndustryComponentData/CaseIndustryData";
+import { useTranslation } from "react-i18next";
 
 const ByCase = () => {
+  const { t } = useTranslation();
   return (
     <div className="w-full max-w-screen-2xl mx-auto flex flex-col px-4 py-2">
       {/* Header */}
-      <span className="text-md text-gray-500 mb-4">Use Cases</span>
+      <span className="text-md text-gray-500 mb-4">{t("use_cases_text")}</span>
 
       {/* Grid */}
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -16,7 +18,7 @@ const ByCase = () => {
             iconbgColor={item.iconbgColor}
             iconColor={item.iconColor}
             Icon={item.IconName}
-            contentText={item.contentText}
+            contentText={t(item.contentText)}
             linkAddress={item.linkAddress}
           />
         ))}
@@ -26,7 +28,7 @@ const ByCase = () => {
       <div className="w-full border-t border-gray-200 my-4" />
 
       {/* Footer */}
-      <span className="text-md text-gray-500 mt-4">See all use cases</span>
+      <span className="text-md text-gray-500 mt-4">{t("see_all_cases")}</span>
     </div>
   );
 };
--- src/components/AdditionScreens/ByIndustry.jsx
+++ src/components/AdditionScreens/ByIndustry.jsx
@@ -1,41 +1,35 @@
-import React from 'react'
-import IconContent from '../IconContent/IconContent';
-import industrieScreensData from '../IndustryComponentData/IndustryScreenData';
-import { useTranslation } from 'react-i18next';
-
-
-
-
+import React from "react";
+import IconContent from "../IconContent/IconContent";
+import industrieScreensData from "../IndustryComponentData/IndustryScreenData";
+import { useTranslation } from "react-i18next";
 
 const ByIndustry = () => {
-   const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
-    <div className='flex flex-col  px-3 h-auto  w-full' >
-        {/* top contetn */}
-        <span className='text-md text-gray-500 my-2'>
-            Industries
-        </span>
-        {/* main content */}
-        <div className="w-full grid grid-cols-2 lg:grid-cols-3 grid-rows-6 gap-1 h-auto">
+    <div className="flex flex-col  px-3 h-auto  w-full">
+      {/* top contetn */}
+      <span className="text-md text-gray-500 my-2">{t("industries_text")}</span>
+      {/* main content */}
+      <div className="w-full grid grid-cols-2 lg:grid-cols-3 grid-rows-6 gap-1 h-auto">
         {industrieScreensData.map((item, idx) => (
-            <IconContent
+          <IconContent
             key={idx}
             iconbgColor={item.iconbgColor}
             iconColor={item.iconColor}
             Icon={item.IconName}
-            contentText={item.contentText}
+            contentText={t(item.contentText)}
             linkAddress={item.linkAddress}
-            />
+          />
         ))}
-        </div>
-        {/* divider */}
-        <div className='w-full border-[1px] border-gray-200 my-2'></div>
-        {/* bottom content */}
-        <span className='text-md text-gray-500 my-2'>
-            See all industries
-        </span>
+      </div>
+      {/* divider */}
+      <div className="w-full border-[1px] border-gray-200 my-2"></div>
+      {/* bottom content */}
+      <span className="text-md text-gray-500 my-2">
+        {t("see_all_industries")}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default ByIndustry
+export default ByIndustry;
